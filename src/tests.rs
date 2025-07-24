b5de1cb0971cdbee939ede6b9a0a658765975906
jimmio <jim@somesaymaybeothersarentsosu.re>
2025-07-24 07:34:22 -0400

    passing test for challenge 5

--- src/tests.rs
+++ src/tests.rs
@@ -112,4 +112,17 @@ mod tests {
             "Now that the party is jumping\n"
         );
     }
+
+    // Challenge 5
+    #[test]
+    fn t_implement_repeating_key_xor() {
+        let input = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+        let input_bytes = string_to_bytes(input);
+        let key = "ICE";
+        let key_bytes = string_to_bytes(key);
+        let xord_bytes = xor_bytes(&input_bytes, &key_bytes);
+        let xord_hex = bytes_to_hex(xord_bytes);
+        let expected = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+        assert_eq!(xord_hex, expected);
+    }
 }
